b28c9c417b4fe71e2a1c4a942f22d91159e169fa
higaguin <higaguin@hotmail.com>
2019-10-03 18:32:29 -0500

    first finish

--- controllers/PoemController.js
+++ controllers/PoemController.js
@@ -5,26 +5,19 @@ let poemController = {};
 
 poemController.random = (req, res) => {
   models.Poem.count().exec((err, count) => {
-    if (err) {
-      res.send({ error: err });
-    } else {
-      res.send(count);
-    }
+    let random = Math.floor(Math.random() * count);
+    //
+    models.Poem.findOne()
+      .skip(random)
+      .populate("poet")
+      .exec((err, poem) => {
+        if (err) {
+          res.send({ error: err });
+        } else {
+          res.send(poem);
+        }
+      });
   });
-  // models.Poem.count().exec((err, count) => {
-  //   let random = Math.floor(Math.random() * count);
-  //   //
-  //   models.Poem.findOne()
-  //     .skip(random)
-  //     .populate("poet")
-  //     .exec((err, poem) => {
-  //       if (err) {
-  //         res.send({ error: err });
-  //       } else {
-  //         res.send(poem);
-  //       }
-  //     });
-  // });
 };
 
 module.exports = poemController;
