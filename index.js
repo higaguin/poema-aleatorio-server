e06182e2440aca9478773e13e9dfd0dc967cee4c
higaguin <higaguin@hotmail.com>
2019-10-03 18:58:34 -0500

    first finish

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("conectado");
-  // seeders();
+  seeders();
 });
 
 app.get("/", (req, res) => {
