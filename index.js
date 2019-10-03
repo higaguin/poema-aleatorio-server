11eb0407de257ea31b1b06f82d18c36a48e284cf
higaguin <higaguin@hotmail.com>
2019-10-03 17:59:16 -0500

    fist finish

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ poemRoutes(app);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
+  console.log("conectado");
   // seeders();
 });
 
